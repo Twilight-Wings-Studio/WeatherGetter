efe174e8bfb6785c1cd549dea6fe2c20b26c4aa1
sy <sy_g@hotmail.co.jp>
2023-12-25 11:14:51 +0900

    Update feed_getter.py

--- src/feed_getter.py
+++ src/feed_getter.py
@@ -18,13 +18,13 @@ class RegularFeedEntry:
 		print(content)
 		print('')
 
-# 長期フィード（定時）
+# 高頻度フィード（定時）
 class RegularFeed:
 	def __init__(self):
 		self.USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
-		self.Feed_Url = 'https://www.data.jma.go.jp/developer/xml/feed/regular_l.xml'
+		self.Feed_Url = 'https://www.data.jma.go.jp/developer/xml/feed/regular.xml'
 		self.Input_File = 'output/feed.xml'
-		self.getAndSaveFeed(self.Input_File)
+		self.getAndSaveFeed(self.Feed_Url, self.Input_File)
 		with open(self.Input_File) as file:
 			self.entries = []
 			self.parseFeed(xmltodict.parse(file.read()))
@@ -35,10 +35,10 @@ class RegularFeed:
 		res = requests.get(url, headers = headers)
 		return res
 
-	def getAndSaveFeed(self, output_file):	
+	def getAndSaveFeed(self, url, output_file):	
 		try:
 			# フィードをダウンロード
-			response = self.downloadFeed(self.Feed_Url)
+			response = self.downloadFeed(url)
 			response.raise_for_status()  # エラーチェック
 			# ダウンロードしたフィードをファイルに保存
 			with open(output_file, 'wb') as file:
@@ -51,6 +51,19 @@ class RegularFeed:
 	def parseFeed(self, dictobj):
 		feed = dictobj['feed']
 		for entry in feed['entry']:
+			if '府県天気予報' entry['title']:
+				newObj = RegularFeedEntry(
+					entry['title'],
+					entry['id'],
+					entry['updated'],
+					entry['author']['name'],
+					entry['link']['@href'],
+					entry['content']['#text'])
+				output_name = basename = os.path.basename(entry['link']['@href'])
+				print(output_name)
+				#getAndSaveFeed(entry['link']['@href'], 'output/' + output_file)
+				self.entries.append(newObj)
+
 			if '府県週間天気予報' in entry['title']:
 				newObj = RegularFeedEntry(
 					entry['title'],
@@ -59,6 +72,9 @@ class RegularFeed:
 					entry['author']['name'],
 					entry['link']['@href'],
 					entry['content']['#text'])
+				output_name = basename = os.path.basename(entry['link']['@href'])
+				print(output_name)
+				#getAndSaveFeed(entry['link']['@href'], 'output/' + output_file)
 				self.entries.append(newObj)
 
 if __name__ == "__main__":
